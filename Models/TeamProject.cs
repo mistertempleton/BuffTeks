cc0f7435cae399bcf68eb6bceb6b24dbed576744
Alexander Roder <ar913509@wtstudents.wtamu.edu>
2018-09-19 12:01:12 -0500

    Additional props

--- Models/TeamProject.cs
+++ Models/TeamProject.cs
@@ -4,7 +4,21 @@ namespace BuffTeks.Models
 {
     public class TeamProject
     {
-        public int TeamID;
+      public int TeamID
+      {
+          get
+          {
+            if (this.TeamID == 3)
+            {
+                return 9;
+            }
+          
+          
+          {
+            return this.TeamID;
+          } 
+      }
+      }
         public Team TheTeam {get; set;}
         public int ProjectID;
 
