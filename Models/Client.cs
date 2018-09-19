75ed08795a8ab32e2610d64fbb8d8b6b7608d013
Alexander Roder <ar913509@wtstudents.wtamu.edu>
2018-09-19 11:45:34 -0500

    stuff n things

---
+++ Models/Client.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace BuffTeks.Models
+{
+    public class Client
+    {
+        // project attributes
+        // id
+        // first name
+        // last name
+        // phone
+        // email
+        // attached organization
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,6 @@
+using System;
+
+namespace BuffTeks.Models
+{
+    
+}
