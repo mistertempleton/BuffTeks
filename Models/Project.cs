17f1fb599e415fcc41f4e0bf9a74ac330b5c45ef
Alexander Roder <ar913509@wtstudents.wtamu.edu>
2018-09-19 11:53:20 -0500

    Started Modeling Process

---
+++ Models/Project.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BuffTeks.Models
+{
+    public class Project
+    {
+        // attribute list
+        // PK
+        // name
+        // leader = FK team lead
+        // client = FK client
+        // deadline - (Formatted as DateTime in C#)
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BuffTeks.Models
+{
+    public class Team 
+    {
+        // attribute list
+        // PK
+        // Student list - PK
+        
+    }
+}
