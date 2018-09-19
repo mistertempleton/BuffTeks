06f9b21ab135aa7d7be257a8821b08cc26c37006
Alexander Roder <ar913509@wtstudents.wtamu.edu>
2018-09-19 12:11:22 -0500

    added inheritance message for future me

--- Models/Student.cs
+++ Models/Student.cs
@@ -2,5 +2,16 @@ using System;
 
 namespace BuffTeks.Models
 {
-    
+    public class Student
+    {
+        // STUDENT AND CLIENT HAVE WHAT IN COMMON (INHERITANCE -- BOTH HUMAN)
+        // attribute list
+        // PK
+        // firstName
+        // lastName
+        // Phone
+        // Email
+        // Role
+    }
+
 }
